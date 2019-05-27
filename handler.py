cc8aef24e349ce9acb3714fca4649662e426396f
Imaduddin A Majid <imaduddinamajid@gmail.com>
2019-05-27 12:59:15 +0300

    Receive URL as Lambda function argument
    Integrate with API Gateway, to receive URL as argument.
    
    Ref: https://github.com/ImaduddinAMajid/emptor-trial-project/issues/2

--- handler.py
+++ handler.py
@@ -1,15 +1,29 @@
 import json
 
 
-def hello(event, context):
+def extract_title(event, context):
+
+    if 'queryStringParameters' not in event:
+        return {'statusCode': 400,
+        'headers': {'Content-Type': 'application/json'},
+        'body': 'No query string is given'}
+
+    if 'url' not in event['queryStringParameters']:
+        return {'statusCode': 400,
+        'headers': {'Content-Type': 'application/json'},
+        'body': 'URL is not given'}
+
+    url = event['queryStringParameters']['url']
+
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": f"Input URL: {url}",
         "input": event
     }
 
     response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(body)
     }
 
     return response
