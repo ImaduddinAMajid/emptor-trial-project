95ab2fc8cbf6a7fc5421e8528275cb88b978c1bc
Imaduddin A Majid <imaduddinamajid@gmail.com>
2019-05-28 13:46:38 +0300

    Make request and extract title
    Make request to the received URL and extract the title of its HTML
    document response. Then return the extracted title to the client.
    
    Ref: https://github.com/ImaduddinAMajid/emptor-trial-project/issues/3, https://github.com/ImaduddinAMajid/emptor-trial-project/issues/4

--- handler.py
+++ handler.py
@@ -1,4 +1,7 @@
 import json
+import re
+
+import requests
 
 
 def extract_title(event, context):
@@ -13,11 +16,34 @@ def extract_title(event, context):
         'headers': {'Content-Type': 'application/json'},
         'body': 'URL is not given'}
 
+    # Get URL from the query string parameters
     url = event['queryStringParameters']['url']
 
+    try:
+        html = requests.get(url)
+
+    except:
+        body = {
+            'message': 'Please enter a valid URL'
+        }        
+        return {'statusCode': 400,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(body)}
+    
+    html = html.text
+
+    title = find_title(html)
+
+    if not title:
+        body = {
+            'message': 'Title not found. Please check your URL.'
+        }
+        return {'statusCode': 400,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(body)}
+
     body = {
-        "message": f"Input URL: {url}",
-        "input": event
+        "title": title
     }
 
     response = {
@@ -27,3 +53,9 @@ def extract_title(event, context):
     }
 
     return response
+
+
+def find_title(html):
+    title_pattern = re.compile('((<title.*?>(.*?)</title>))', re.I|re.S)
+    match = title_pattern.search(html)
+    return match.group(3)
